15f0d89dad4dc5c17f0498fb25ad7080ccb28d86
Michael Angelo <nwo1318@gmail.com>
2025-08-06 17:17:02 +0800

    Initial commit

---
+++ veterinary-pet-system/src/main/java/com/codecraft/veterinary_pet_system/VeterinaryPetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.codecraft.veterinary_pet_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryPetSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryPetSystemApplication.class, args);
+	}
+
+}
---
+++ veterinary-pet-system/src/test/java/com/codecraft/veterinary_pet_system/VeterinaryPetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecraft.veterinary_pet_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryPetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
